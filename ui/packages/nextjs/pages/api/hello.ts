91db65c0935b04f0f6a41b175cc521fcdf7b4735
tommyzbear <ajdsenwill@hotmail.com>
2023-10-28 21:52:23 +0100

    Login Wrapper

---
+++ ui/packages/nextjs/pages/api/hello.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  res.status(200).json({ text: "Hello" });
+}
