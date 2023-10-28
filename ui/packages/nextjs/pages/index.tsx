7594c6aa449f95fc01209242c63fb76e6353afed
azul <1337+morkeltry@users.noreply.github.com>
2023-10-28 21:20:31 +0100

    etherspot's PersonalAccountImplementationV1 compiles, deploys and initialises on chain 31337

--- ui/packages/nextjs/pages/index.tsx
+++ ui/packages/nextjs/pages/index.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
+import { EtherspotTransactionKit } from "@etherspot/transaction-kit";
+// import { ethers } from "ethers";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
@@ -31,30 +33,34 @@ const Home: NextPage = () => {
           </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+        <EtherspotTransactionKit
+          provider={providerWallet} /* The random wallet we created above */
+        ></EtherspotTransactionKit>
+          <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+            <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
+              <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+                <BugAntIcon className="h-8 w-8 fill-secondary" />
+                <p>
+                  Tinker with your smart contract using the{" "}
+                  <Link href="/debug" passHref className="link">
+                    Debug Contract
+                  </Link>{" "}
+                  tab.
+                </p>
+              </div>
+              <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+                <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+                <p>
+                  Explore your local transactions with the{" "}
+                  <Link href="/blockexplorer" passHref className="link">
+                    Block Explorer
+                  </Link>{" "}
+                  tab.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </EtherspotTransactionKit>
       </div>
     </>
   );
