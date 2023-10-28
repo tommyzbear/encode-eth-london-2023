40e51efd61d42862f35813f20ee2adeb3ea95149
saidam90 <saida.murtazaliyeva@yahoo.com>
2023-10-28 15:40:19 +0100

    Add 3d model

--- ui/packages/nextjs/components/Header.tsx
+++ ui/packages/nextjs/components/Header.tsx
@@ -26,6 +26,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/nfts",
   },
   {
+    label: "Avatar",
+    href: "/avatar",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
