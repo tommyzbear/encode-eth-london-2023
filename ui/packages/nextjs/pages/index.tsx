cf4cb7addeab2c355d4d9b5380898f3207d7c663
Antonio Furtado <antoniordf@hotmail.com>
2023-10-29 11:09:12 +0000

    deleted y

--- ui/packages/nextjs/pages/index.tsx
+++ ui/packages/nextjs/pages/index.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
           </Link>{" "}
           <Link href="/recovery" passHref>
             <button className="h-10 px-5 text-indigo-700 transition-colors duration-150 border border-indigo-500 rounded-lg focus:shadow-outline hover:bg-indigo-500 hover:text-indigo-100">
-              Recovery
+              Recover
             </button>
           </Link>{" "}
         </div>
