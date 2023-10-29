0682ad81f0d4ec63ec69f86f600ef92e7719a64f
ariel lima <ariel_lima_oliveira@yahoo.com.br>
2023-10-29 10:30:51 +0000

    fixed dropdown wallet menu colour

--- ui/packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
+++ ui/packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton.tsx
@@ -126,7 +126,7 @@ export const RainbowKitCustomConnectButton = () => {
                     </label>
                     <ul
                       tabIndex={0}
-                      className="dropdown-content menu z-[2] p-2 mt-2 shadow-center shadow-accent bg-base-200 rounded-box gap-1"
+                      className="dropdown-content menu z-[2] p-2 mt-2 shadow-center shadow-accent bg-base-200 text-black rounded-box gap-1"
                     >
                       <li>
                         {addressCopied ? (
