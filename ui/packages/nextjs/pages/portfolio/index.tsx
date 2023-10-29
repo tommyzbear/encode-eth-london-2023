73f37bb8f597705e410d187fcbb876c95f1ef3c9
Antonio Furtado <54672689+antoniordf@users.noreply.github.com>
2023-10-29 05:08:43 +0000

    Update index.tsx

--- ui/packages/nextjs/pages/portfolio/index.tsx
+++ ui/packages/nextjs/pages/portfolio/index.tsx
@@ -58,9 +58,9 @@ const Portfolio: React.FC = () => {
   return (
     <div>
       <Wheel assets={assetData || []} />
-      {assetData?.map((asset, index) => (
-        <AssetCard key={index} {...asset} />
-      ))}
+        {assetData?.map((asset, index) => (
+          <AssetCard key={index} {...asset} />
+        ))}
       {/* <Chart data={assetData} /> */}
     </div>
   );
